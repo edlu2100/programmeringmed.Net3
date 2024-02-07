00e85b353b5fa37633d8181dab798e72ec659c0f
edlu2100 <edwinlundback@gmail.com>
2024-02-07 19:26:12 +0100

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace programmeringmed.Net3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
